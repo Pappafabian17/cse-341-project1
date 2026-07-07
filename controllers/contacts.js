328f7c2c1ff04bdbb0c74390dff95ef64638ae94
Fabian Pappa <fabian.pappa.ext@errepar.com>
2026-07-06 21:02:35 -0300

    installed swagger

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags = ['Contacts']
   const result = await mongodb.getDatabase().db().collection("contacts").find();
   result.toArray().then((contacts) => {
     res.setHeader("Content-Type", "application/json");
@@ -9,12 +10,14 @@ const getAll = async (req, res) => {
   });
 };
 const getSingle = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
+  //#swagger.tags = ['Contacts']
+
+  const contactId = new ObjectId(req.params.id);
   const result = await mongodb
   .getDatabase()
   .db()
   .collection("contacts")
-  .find({ _id: userId });
+  .find({ _id: contactId });
   result.toArray().then((contacts) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(contacts[0]);
@@ -22,6 +25,8 @@ const getSingle = async (req, res) => {
 };
 
 const createContact = async (req, res) => {
+  //#swagger.tags = ['Contacts']
+
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -37,7 +42,9 @@ const createContact = async (req, res) => {
   }
 };
 const updateContact = async (req, res) => {
-   const userId = new ObjectId(req.params.id);
+  //#swagger.tags = ['Contacts']
+
+   const contactId = new ObjectId(req.params.id);
    const contact = {
      firstName: req.body.firstName,
      lastName: req.body.lastName,
@@ -45,7 +52,7 @@ const updateContact = async (req, res) => {
      favoriteColor: req.body.favoriteColor,
      birthday: req.body.birthday
     }
-    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId}, contact);
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId}, contact);
     if (response.modifiedCount > 0 || response.matchedCount > 0) {
       res.status(204).send();
     } else {
@@ -53,8 +60,9 @@ const updateContact = async (req, res) => {
   }
 };
 const deleteContact = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId });
+  //#swagger.tags = ['Contacts']
+  const contactId = new ObjectId(req.params.id);
+  const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: contactId });
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
-// router.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
+router.get("/", (req, res) => {
+  //#swagger.tags = ['Hello world']
+  res.send("Hello world");
+});
 
+router.use("/", require("./swagger"));
 router.use("/contacts", require("./contacts"));
 
 module.exports = router;
